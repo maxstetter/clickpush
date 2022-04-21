52df54d7818a1c586aaa1fe0b7bd9f2155a8a8cc
Max <Max>
2022-04-21 11:50:44 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+var app = new Vue({
+  el: '#game',
+  data: {
+    rope: 50,
+    winner: "",
+  },
+  methods: {
+	  connectSocket: function () {
+		this.socket = new WebSocket("ws://localhost:8080");
+		//on message send data.
+		this.socket.onmessage = (event) => {
+			console.log("socket.onmessage worked.");
+      console.log("this is the message: " + event.data);
+			var message = JSON.parse(event.data);
+			this.rope = message.rope;
+      this.winner = message.winner;
+		};
+	},
+    click: function (team) {
+      console.log('team', team);
+	    var message = {action: 'click', team: team};
+	    this.socket.send(JSON.stringify(message));
+      this.rope = message.rope;
+    },
+    reset: function () {
+      console.log('reset clicked');
+	    var message = {action: 'reset'};
+	    this.socket.send(JSON.stringify(message));
+    }
+  },
+  created: function () {
+    console.log('ready');
+  	this.connectSocket();
+  }
+});
---
+++ index.js
@@ -0,0 +1,79 @@
+//import { client } from 'websocket';
+//import WebSocket from 'ws';
+//const WebSocketServer = require('ws');
+import { WebSocketServer } from 'ws';
+const wss = new WebSocketServer({ port: 8080 })
+
+var player1 = null;
+var player2 = null;
+var rope = 50;
+var CLIENTS = [];
+var winner = "";
+
+function sendAll(){
+    console.log("sendAll called.");
+    for (var i=0; i < CLIENTS.length; i++){
+        var roper = {rope: rope};
+        var message = {rope: rope, winner: winner}
+        CLIENTS[i].send()
+    }
+}
+
+wss.on('connection', function connection(ws) {
+    if(player1 == null){
+        player1 = ws;
+        ws.send(JSON.stringify("you are Player 1"));
+        player1.on('close', function () {
+        player1 = null;
+        CLIENTS.push(ws);
+    })
+    } else if (player2 == null) {
+        player2 = ws;
+        ws.send(JSON.stringify("you are Player 2"));
+        player2.on('close', function () {
+        player2 = null;
+        CLIENTS.push(ws);
+    })
+    } else {
+        ws.send(JSON.stringify("there are already 2 players."));
+        CLIENTS.push(ws);
+    }
+});
+
+
+wss.on('connection', function (client) {
+    client.on('message', function (message){
+        var data = JSON.parse(message);
+        console.log("data.action: " + data.action);
+        console.log("data.team: " + data.team);
+        if (data.action){
+            if (data.action == "click"){
+                if (player1 == client || player2 == client){
+                    if (data.team == "blue"){
+                        rope += 1;
+                        console.log("rope: " + rope);
+                        var roper = {rope: rope};
+                        if (rope >= 100){
+                            var winner = {winner: "blue"}
+                        }
+                    } else if (data.team == "red"){
+                        rope -= 1;
+                        console.log("rope: " + rope);
+                        var roper = {rope: rope};
+                        if (rope <= 0){
+                            var winner = {winner: "red"}
+                        }
+                    } 
+                }
+            } else if (data.action == "reset"){
+                rope = 50;
+                console.log("rope successfully reset.");
+                var winner = {winner: "none", rope: rope}
+            }
+        }
+        wss.clients.forEach(function each(client){
+            client.send(JSON.stringify(roper));
+            client.send(JSON.stringify(winner));
+        });
+    });
+});
